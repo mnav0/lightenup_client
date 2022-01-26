5dccb7025a0f044eef040a2d89f8e58e58e6c1d5
samliuu <samanthaliu113@gmail.com>
2022-01-26 15:41:32 -0500

    changes to header component.

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,12 +1,11 @@
 import React from "react";
 import { StyledHeader } from "./header.styles";
 
-const Header = ({ text, back }) => {
-
+const Header = ({text, subtext, back}) => {
     return (
       <StyledHeader>
           <h1>{text}</h1>
-           <p>{text}</p>
+          <p>{subtext}</p>
       </StyledHeader>
     );
   };
