1c69399ef506ab6c7bcd6b88c36fab8cbc01c76e
Maggie Navracruz <mnavracruz@gmail.com>
2022-02-01 15:20:25 -0500

    fix formatting

--- src/services/queueService.js
+++ src/services/queueService.js
@@ -4,21 +4,16 @@ export const getQueue = () =>
     fetch(`${URL}/getQueue`)
     .then(response => response.json())
 
-
 export const addToQueue = (typeId) =>
     fetch(`${URL}/addToQueue/${typeId}`)
         .then(response => {
             return response.json()
     })
 
-
-
 export const deleteFromQueue = (animId) =>
     fetch(`${URL}/deleteFromQueue/${animId}`)
         .then(response => {
             return response.json()
     })
-
-
 
 export default { getQueue, addToQueue, deleteFromQueue }
