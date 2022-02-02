8abca31f9ecaab1141ba056059291e816a5ab7bf
samliuu <samanthaliu113@gmail.com>
2022-02-02 14:48:10 -0500

    Changes to research page.

--- src/components/colorBlock/colorBlock.js
+++ src/components/colorBlock/colorBlock.js
@@ -2,17 +2,16 @@ import React from "react";
 import { StyledColorBlock } from "./colorBlock.styles";
 import { Text } from "../styled";
 
-const ColorBlock = ({ text, string1, string2, string3, string4 }) => {
+const ColorBlock = ({ emotion, color1, color2, color3, color4 }) => {
   return (
     <StyledColorBlock>
-      <Text>{text}</Text>
-      <div style={{backgroundColor: string1}}></div>
-      <div style={{backgroundColor: string2}}></div>
-      <div style={{backgroundColor: string3}}></div>
-      <div style={{backgroundColor: string4}}></div>
+      <Text>{emotion}</Text>
+      <div style={{backgroundColor: color1}}></div>
+      <div style={{backgroundColor: color2}}></div>
+      <div style={{backgroundColor: color3}}></div>
+      <div style={{backgroundColor: color4}}></div>
     </StyledColorBlock>
   );
 };
-
 
 export default ColorBlock;
--- src/researchPage.js
+++ src/researchPage.js
@@ -14,56 +14,70 @@ function ResearchPage() {
         }
       />
       <ColorBlock
-        text={"relax, de-stress, calm, attention"}
-        string1={"#D9E5F3"}
-        string2={"#B2D3ED"}
-        string3={"#B2B5D9"}
-        string4={"#296CA0"}
+        emotion={"relax, de-stress, calm, attention"}
+        color1={"#D9E5F3"}
+        color2={"#B2D3ED"}
+        color3={"#B2B5D9"}
+        color4={"#296CA0"}
       />
       <ColorBlock
-        text={"healing, pain relief, comfort"}
-        string1={"#E7F9C3"}
-        string2={"#ACDCD1"}
-        string3={"#97CD80"}
-        string4={"#719680"}
+        emotion={"healing, pain relief, comfort"}
+        color1={"#E7F9C3"}
+        color2={"#ACDCD1"}
+        color3={"#97CD80"}
+        color4={"#719680"}
       />
       <ColorBlock
-        text={"joy, energy, positivity"}
-        string1={"#FFFFA2"}
-        string2={"#F6E7A4"}
-        string3={"#FEBB3D"}
-        string4={"#D46C00"}
+        emotion={"joy, energy, positivity"}
+        color1={"#FFFFA2"}
+        color2={"#F6E7A4"}
+        color3={"#FEBB3D"}
+        color4={"#D46C00"}
       />
       <ColorBlock
-        text={"creativity, inspiration"}
-        string1={"#FDE8C0"}
-        string2={"#CFC8F8"}
-        string3={"#FCC663"}
-        string4={"#70669D"}
+        emotion={"creativity, inspiration"}
+        color1={"#FDE8C0"}
+        color2={"#CFC8F8"}
+        color3={"#FCC663"}
+        color4={"#70669D"}
       />
       <ColorBlock
-        text={"strength, courage, focus"}
-        string1={"#FFA5A4"}
-        string2={"#D78F8E"}
-        string3={"#BABABA"}
-        string4={"#565656"}
+        emotion={"strength, courage, focus"}
+        color1={"#FFA5A4"}
+        color2={"#D78F8E"}
+        color3={"#BABABA"}
+        color4={"#565656"}
       />
       <p>Sources</p>
       <p>
-        Says, Deborah, et al. “Color Psychology: The Psychological Effects of
-        Colors.” Art Therapy, 24 Dec. 2012,
-        http://www.arttherapyblog.com/online/color-psychology-psychologica-effects-of-colors/#.YYLxU73MK58.{" "}
+        <a
+          href={
+            "http://www.arttherapyblog.com/online/color-psychology-psychologica-effects-of-colors/#.YYLxU73MK58."
+          }
+        >
+          {" "}
+          Says, Deborah, et al. “Color Psychology: The Psychological Effects of
+          Colors.” Art Therapy, 24 Dec. 2012,
+        </a>
       </p>
       <p>
-        Sevinc Kurt, Kelechi Kingsley Osueke. “The Effects of Color on the Moods
-        of College Students - Sevinc Kurt, Kelechi Kingsley Osueke, 2014.” SAGE
-        Journals,
-        https://journals.sagepub.com/doi/full/10.1177/2158244014525423.{" "}
+        <a
+          href={
+            "https://journals.sagepub.com/doi/full/10.1177/2158244014525423"
+          }
+        >
+          {" "}
+          Sevinc Kurt, Kelechi Kingsley Osueke. “The Effects of Color on the
+          Moods of College Students - Sevinc Kurt, Kelechi Kingsley Osueke,
+          2014.” SAGE Journals,
+        </a>
       </p>
       <p>
-        Cherry, Kendra. “Can Color Affect Your Mood and Behavior?” Verywell
-        Mind, Verywell Mind, 28 May 2020,
-        https://www.verywellmind.com/color-psychology-2795824.{" "}
+        <a href= {"https://www.verywellmind.com/color-psychology-2795824"}>
+          {" "}
+          Cherry, Kendra. “Can Color Affect Your Mood and Behavior?” Verywell
+          Mind, Verywell Mind, 28 May 2020,
+        </a>
       </p>
       <div>
         <Link to="/">
