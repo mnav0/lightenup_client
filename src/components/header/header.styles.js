d61e4a9197579ca523ad01c216a5ff6ae34ed7d6
samliuu <samanthaliu113@gmail.com>
2022-02-08 22:44:27 -0500

    edits to styles.

--- src/components/header/header.styles.js
+++ src/components/header/header.styles.js
@@ -3,22 +3,16 @@ import styled from "styled-components";
 const StyledHeader = styled.header`
   background: transparent;
   color: #003D52;
-  margin: 2em 0em;
-  padding: 1em 0em;
+  margin: 1em 0 0 0;
 
   // Large devices (laptop, 992px and up)
   @media (min-width: 992px) {
-    width: 80%
+    width: 80%;
   }
 
   // Medium devices (tablets, 768px and up)
   @media (min-width: 768px) {
     width: 90%;
-  }
-
-  // Small devices (landscape phones, 576px and up)
-  @media (min-width: 576px) {
-    font-size: 42px;
   }
 `;
 
