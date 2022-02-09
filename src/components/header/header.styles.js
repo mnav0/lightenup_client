a7f60036efb98eb26c02e4d9dd046be98b7c54ba
samliuu <samanthaliu113@gmail.com>
2022-02-08 22:05:23 -0500

    updated header styles.

--- src/components/header/header.styles.js
+++ src/components/header/header.styles.js
@@ -7,16 +7,19 @@ const StyledHeader = styled.header`
   padding: 1em 0em;
 
   // Large devices (laptop, 992px and up)
-  @media (min-width: 99px) {
+  @media (min-width: 992px) {
     width: 80%
   }
 
   // Medium devices (tablets, 768px and up)
   @media (min-width: 768px) {
-    width: 90%
+    width: 90%;
   }
 
   // Small devices (landscape phones, 576px and up)
+  @media (min-width: 576px) {
+    font-size: 42px;
+  }
 `;
 
 export { StyledHeader };
