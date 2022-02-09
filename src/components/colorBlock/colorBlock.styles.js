db5ed302fbbcf0cd6004d78bb2406837e1123d2b
samliuu <samanthaliu113@gmail.com>
2022-02-08 22:44:27 -0500

    edits to styles.

--- src/components/colorBlock/colorBlock.styles.js
+++ src/components/colorBlock/colorBlock.styles.js
@@ -8,32 +8,21 @@ const StyledColorBlock = styled.div`
 
   div {
     display: inline-block;
-    width: 100px;
+    width: 80px;
     height: 64px;
   }
 
-  // Large devices (desktops, 992px and up)
-  @media (min-width: 992px) {
-    width: 50%;
-
   // Medium devices (tablets, 768px and up)
   @media (min-width: 768px) {
-    width: 50%
+    width: 50%;
   }
 `;
 
 const BlockContainer = styled.div`
  width: 100%;
- height: 700px;
  display: flex;
  flex-wrap: wrap;
  justify-content: space-between;
-
- // Large devices (desktops, 992px and up)
- @media (min-width: 992px) {
-   height: 700px;
- }
-
  `;
 
 export { StyledColorBlock, BlockContainer };
